8746da58e266b48a1ea8a62193a0a7f1f626d00e
0xySan <ethtaquet@gmail.com>
2025-04-01 16:41:35 +0200

    added choice to add to zshrc/bashrc

--- install.sh
+++ install.sh
@@ -18,4 +18,18 @@ chmod +x "$_HOME_DIR/bin/42fetch"
 
 sed -i '8i _CONFIG_FOLDER="$_HOME_DIR/.config/42fetch"' $_HOME_DIR/bin/42fetch
 
+echo "Would you like to run 42fetch everytime you launch a term? (y/N)"
+
+read input
+
+if [ "$input" = "y" ]; then
+    if [ -e "$_HOME_DIR/.zshrc" ]; then
+        echo "42fetch" >> "$_HOME_DIR/.zshrc"
+        echo "42fetch added to .zshrc"
+    elif [ -e "$_HOME_DIR/.bashrc" ]; then
+        echo "42fetch" >> "$_HOME_DIR/.bashrc"
+        echo "42fetch added to .bashrc"
+    fi
+fi
+
 echo "42fetch installed successfully!"

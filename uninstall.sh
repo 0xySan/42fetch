bde6e3244964f5510c5d55cdb8205ffa0f69f1fc
0xySan <ethtaquet@gmail.com>
2025-04-01 17:44:43 +0200

    uninstall works in zshrc/bashrc

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,7 @@ _HOME_DIR=$(eval echo "~")
 rm -rf "$_HOME_DIR/bin/42fetch"
 rm -rf "$_HOME_DIR/.config/42fetch"
 
+sed -i '/^42fetch/d' ~/.zshrc
+sed -i '/^42fetch/d' ~/.bashrc
+
 echo "42fetch uninstalled successfully!"

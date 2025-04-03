1ec5ba9fa14fbc256ea5a844c3bd3735b04d1bb4
0xySan <ethtaquet@gmail.com>
2025-04-03 20:31:01 +0200

    install fix

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 _HOME_DIR=$(eval echo "~")
 
+if [ -z "$_HOME_DIR" ]; then
+	echo "Error: HOME_DIR is not set."
+	exit 1
+fi
+
+if [ -f "$_HOME_DIR/bin/42fetch" ]; then
+	echo "42fetch is already installed. Update it or uninstall it first."
+	exit 0
+fi
+
 [ -e "$_HOME_DIR/bin" ] || mkdir "$_HOME_DIR/bin"
 
 if [ -e "$_HOME_DIR/.config" ]; then
@@ -9,7 +19,7 @@ else
 	mkdir "$_HOME_DIR/.config/42fetch"
 fi
 
-if [ -n "$CURL" ] && [ "$CURL" = "true" ]; then
+if [ -n "$CURL" ] && [ "$CURL" = "true" ] || [ -z $(pwd | grep 42fetch) ]; then
 	git clone https://github.com/0xySan/42fetch.git tmp
 	cp -r tmp/data "$_HOME_DIR/.config/42fetch"
 	cp -r tmp/logo "$_HOME_DIR/.config/42fetch"
@@ -25,11 +35,15 @@ chmod +x "$_HOME_DIR/bin/42fetch"
 
 sed -i '8i _CONFIG_FOLDER="$_HOME_DIR/.config/42fetch"' $_HOME_DIR/bin/42fetch
 
-echo "Would you like to run 42fetch everytime you launch a term? (y/N)"
+if [ -n "$CURL" ] && [ "$CURL" = "true" ]; then
+	echo "Using curl to download 42fetch, cannot read input from terminal.\n If you want to use 42fetch everytime you launch a term, please add it to your .zshrc or .bashrc manually."
+else
+	echo "Would you like to run 42fetch everytime you launch a term? (y/N) default: yes"
+fi
 
 read input
 
-if [ "$input" = "y" ]; then
+if [ "$input" = "y" ] || [ "$input" = "" ]; then
 	if [ -e "$_HOME_DIR/.zshrc" ]; then
 		echo "42fetch" >> "$_HOME_DIR/.zshrc"
 		echo "42fetch added to .zshrc"
